719bb5918cfa78c74b37c360c39ec5eccf69af69
ArmanGrigorian <armangrigorian.work@gmail.com>
2023-10-19 15:06:36 +0400

    fixed sorting A - Z - A

--- src/components/sortPanel/SortPanel.tsx
+++ src/components/sortPanel/SortPanel.tsx
@@ -29,8 +29,8 @@ const SortPanel = () => {
 			}}>
 			<option value={"price asc"}>Price &#40; Low - High &#41;</option>
 			<option value={"price desc"}>Price &#40; High - Low &#41;</option>
-			<option value={"model asc"}>Name &#40; A - Z &#41;</option>
-			<option value={"model desc"}>Name &#40; Z - A &#41;</option>
+			<option value={"model desc"}>Name &#40; A - Z &#41;</option>
+			<option value={"model asc"}>Name &#40; Z - A &#41;</option>
 			<option value={"release asc"}>Release &#40; old first &#41;</option>
 			<option value={"release desc"}>Release &#40; new first &#41;</option>
 			<option value={"rating asc"}>Rating &#40; from lowest &#41;</option>
