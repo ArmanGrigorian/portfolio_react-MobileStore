aa2116c4f43db10867e070cf4aab1eec6b47d945
ArmanGrigorian <armangrigorian.work@gmail.com>
2024-03-09 19:38:52 +0400

    few fixes
    new file:   .env
    modified:   package-lock.json
    modified:   package.json
    modified:   src/components/singleItem/SingleItem.scss
    modified:   src/components/storeItem/StoreItem.scss
    modified:   src/components/storePage/StorePage.scss
    modified:   src/components/storePage/StorePage.tsx
    modified:   src/main.scss

--- src/components/storePage/StorePage.tsx
+++ src/components/storePage/StorePage.tsx
@@ -1,10 +1,12 @@
-import "./StorePage.scss";
-import { T_SingleItem } from "../../types/types.ts";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { useAppSelector } from "../../redux/hooks.ts";
 import { selectProducts } from "../../redux/slices/productsSlice.ts";
-import { StoreItem, ItemSkeleton, Pagination, AuxNav } from "../index.ts";
+import { T_SingleItem } from "../../types/types.ts";
+import { AuxNav, ItemSkeleton, Pagination, StoreItem } from "../index.ts";
+import "./StorePage.scss";
 
 const StorePage = () => {
+	const [parent] = useAutoAnimate();
 	const { isPending, storeItems } = useAppSelector(selectProducts);
 
 	return (
@@ -13,7 +15,7 @@ const StorePage = () => {
 
 			<h2>Store</h2>
 
-			<div className={"StorePage__items"}>
+			<div className={"StorePage__items"} ref={parent}>
 				{!isPending
 					? storeItems?.map((item: T_SingleItem) => <StoreItem key={item.id} item={item} />)
 					: [...new Array(6)].map(() => <ItemSkeleton key={crypto.randomUUID()} />)}
